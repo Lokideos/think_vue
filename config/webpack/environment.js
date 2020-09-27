0d820262437292fcb152eab4b93bd146791f2ec8
lokideos <archmage09@gmail.com>
2020-09-27 14:21:57 +0300

    Add pug-loader & saas loader to webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,11 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const pugLoader = require('./loaders/pug')
+const saasLoader = require('./loaders/saas')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('pug', pugLoader)
+environment.loaders.prepend('saas', saasLoader)
 module.exports = environment
