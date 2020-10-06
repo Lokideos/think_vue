025b3791367ec81f0aff989d73a30fe4a8560d51
lokideos <archmage09@gmail.com>
2020-10-06 20:41:34 +0300

    Add endpoint for backend validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,22 @@
 
 class Client < User
   validates :fullname, :phone, presence: true
+
+  def self.validate_param(params)
+    validation_type = params.keys.first
+    case validation_type
+    when 'email' then validate_email_uniqueness(params['email'])
+    when 'phone' then validate_phone_uniqueness(params['phone'])
+    else
+      false
+    end
+  end
+
+  def self.validate_email_uniqueness(email)
+    !Client.exists?(email: email)
+  end
+
+  def self.validate_phone_uniqueness(phone)
+    !Client.exists?(phone: phone)
+  end
 end
