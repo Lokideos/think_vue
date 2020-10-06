0f70b312f0a53d2709e1dd3064423402f945f1d7
lokideos <archmage09@gmail.com>
2020-10-06 20:48:51 +0300

    Add tests for client backend validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,21 +3,23 @@
 class Client < User
   validates :fullname, :phone, presence: true
 
-  def self.validate_param(params)
-    validation_type = params.keys.first
-    case validation_type
-    when 'email' then validate_email_uniqueness(params['email'])
-    when 'phone' then validate_phone_uniqueness(params['phone'])
-    else
-      false
+  class << self
+    def validate_param(params)
+      validation_type = params.keys.first
+      case validation_type
+      when 'email' then validate_email_uniqueness(params['email'])
+      when 'phone' then validate_phone_uniqueness(params['phone'])
+      else
+        false
+      end
     end
-  end
 
-  def self.validate_email_uniqueness(email)
-    !Client.exists?(email: email)
-  end
+    def validate_email_uniqueness(email)
+      !Client.exists?(email: email)
+    end
 
-  def self.validate_phone_uniqueness(phone)
-    !Client.exists?(phone: phone)
+    def validate_phone_uniqueness(phone)
+      !Client.exists?(phone: phone)
+    end
   end
 end
